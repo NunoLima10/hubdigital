005a5e18b99405d5b6b017fe532041ff52812b39
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2025-06-15 21:04:38 -0100

    feat(web): add new theme colors

--- web/src/app/provider.tsx
+++ web/src/app/provider.tsx
@@ -3,9 +3,8 @@ import "@mantine/core/styles.css";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { shadcnCssVariableResolver } from "./cssVariableResolver";
 import { queryClient } from "./query-client";
-import { shadcnTheme } from "./theme";
+import { theme } from "./theme";
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -13,10 +12,7 @@ type AppProviderProps = {
 
 export function AppProvider({ children }: AppProviderProps) {
   return (
-    <MantineProvider
-      theme={shadcnTheme}
-      cssVariablesResolver={shadcnCssVariableResolver}
-    >
+    <MantineProvider theme={theme}>
       <Notifications autoClose={5000} position="top-center" />
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </MantineProvider>
